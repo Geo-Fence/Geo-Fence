e421645e07011cdf49d6f8a6a57254efd4182aa6
David Struewing <david.struewing@gmail.com>
2014-10-30 09:45:17 -0400

    Added Javadoc to API functions

--- RollCall/src/edu/temple/rollcall/util/API.java
+++ RollCall/src/edu/temple/rollcall/util/API.java
@@ -46,6 +46,13 @@ public class API  {
         return response.toString();
     }
 	
+	/**
+	 * Get all upcoming sessions for the specified student.
+	 * @param context
+	 * @param student_id
+	 * @return JSON Array of all upcoming sessions
+	 * @throws Exception
+	 */
 	public static JSONArray getSessionsForStudent(Context context, String student_id) throws Exception {
 		
 		String response = makeAPICall(context, "getsessionsforstudent.php?student_id=" + student_id);
@@ -61,6 +68,14 @@ public class API  {
 		return null;
 	}
 	
+	/**
+	 * Check in the specified student to the specified session
+	 * @param context
+	 * @param student_id
+	 * @param session_id
+	 * @return TRUE if the check in was successful, FALSE if the student has already checked in.
+	 * @throws Exception
+	 */
 	public static boolean checkIn(Context context, String student_id, String session_id) throws Exception {
 		String response = makeAPICall(context, "checkin.php?student_id=" + student_id + "&session_id=" + session_id);
 		if(response.equals("success")) {
@@ -69,6 +84,14 @@ public class API  {
 		return false;
 	}
 	
+	/**
+	 * Check out the specified student from the specified session
+	 * @param context
+	 * @param student_id
+	 * @param session_id
+	 * @return TRUE if the check out was successful, FALSE if the student never checked in.
+	 * @throws Exception
+	 */
 	public static boolean checkOut(Context context, String student_id, String session_id) throws Exception {
 		String response = makeAPICall(context, "checkout.php?student_id=" + student_id + "&session_id=" + session_id);
 		if(response.equals("success")) {
