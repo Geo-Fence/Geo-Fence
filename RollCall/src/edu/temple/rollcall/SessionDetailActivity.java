374cde3562eb156942b990a1daaafbbf2116b4ec
David Struewing <david.struewing@gmail.com>
2014-11-05 01:16:42 -0500

    Renamed InfoPage as SessionDetailActivity and added back button to the action bar.

--- RollCall/src/edu/temple/rollcall/SessionDetailActivity.java
+++ RollCall/src/edu/temple/rollcall/SessionDetailActivity.java
@@ -1,18 +1,27 @@
 package edu.temple.rollcall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SessionDetailActivity extends Activity {
 
+	TextView tvInfo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_session_detail);
 		
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		tvInfo = (TextView) findViewById(R.id.info_name);
+
+		Intent intent = getIntent();
+		tvInfo.setText(intent.getExtras().getString("coursename"));
 	}
 	
 	@Override
