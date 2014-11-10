ba1abd545729e785dfe47dcec926e95ecce1766d
David Struewing <david.struewing@gmail.com>
2014-11-10 10:16:01 -0500

    Fixed bugs in card countdown.

--- RollCall/src/edu/temple/rollcall/cards/Card.java
+++ RollCall/src/edu/temple/rollcall/cards/Card.java
@@ -1,20 +1,19 @@
 package edu.temple.rollcall.cards;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.joda.time.DateTime;
 import org.json.JSONObject;
 
 import android.os.CountDownTimer;
-import android.view.View;
-import android.widget.TextView;
-
 
 public class Card {
 	public static final String thumbURLPrefix = "https://maps.googleapis.com/maps/api/staticmap?&size=200x200&zoom=17&center=";
 
 	public JSONObject sessionInfo;
 	public long start_time_millis;
+	public long end_time_millis;
 	public String thumbURL;
     public String course_name;
     public String teacher_name;
@@ -22,10 +21,11 @@ public class Card {
     public String start_time;
     public String end_time;
     
+    public CardCountDownTimer timer;
+        
     public Card(JSONObject sessionInfo) {
     	this.sessionInfo = sessionInfo;
     	try {
-    		this.start_time_millis = Long.parseLong(sessionInfo.getString("start_time")) * 1000;
     		this.thumbURL = thumbURLPrefix + sessionInfo.getString("lat") + "," + sessionInfo.getString("lng");
     		this.course_name = sessionInfo.getString("course_name");
     		this.teacher_name = sessionInfo.getString("teacher_first_name") + " " + sessionInfo.getString("teacher_last_name");
@@ -36,8 +36,50 @@ public class Card {
     		
     		dateTime = dateTime.withMillis(Long.parseLong(sessionInfo.getString("end_time")) * 1000);
     		this.end_time = dateTime.toString("hh:mm a");
+    		    		
+    		this.start_time_millis = Long.parseLong(sessionInfo.getString("start_time")) * 1000;
+    		this.end_time_millis = Long.parseLong(sessionInfo.getString("end_time")) * 1000;
+    		
+    		this.timer = new CardCountDownTimer(this.start_time_millis - new Date().getTime(), 1000);
       	} catch (Exception e) {
     		e.printStackTrace();
+    	}
+    }
+    
+    public class CardCountDownTimer extends CountDownTimer {
+
+    	public String output;
+
+    	public CardCountDownTimer(long millisInFuture, long countDownInterval) {
+    		super(millisInFuture, countDownInterval);
+    		this.start();
+    	}
+
+    	@Override
+    	public void onTick(long millisUntilFinished) {
+    		Long millis = millisUntilFinished;
+    		long days = TimeUnit.MILLISECONDS.toDays(millis);
+    		millis -= TimeUnit.DAYS.toMillis(days);
+    		long hours = TimeUnit.MILLISECONDS.toHours(millis);
+    		millis -= TimeUnit.HOURS.toMillis(hours);
+    		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+    		if(days == 0) {
+    			String message = "Begins in";
+    			if(hours > 1) message += " " + hours + " hours";
+    			else if(hours == 1) message += " " + hours + " hour";
+    			if(minutes > 1) message += " " + minutes + " minutes.";
+    			else if(minutes == 1) message += " " + minutes + " minute.";
+    			else if(minutes == 0) message += ".";
+    			if(hours == 0 && minutes == 0) message += " less than a minute.";
+    			this.output = message;
+    		} else {
+    			this.output = "";
+    		}
+    	}
+
+    	@Override
+    	public void onFinish() {
+    		this.output = "In progress...";
     	}
     }
 }
--- RollCall/src/edu/temple/rollcall/cards/CardListAdapter.java
+++ RollCall/src/edu/temple/rollcall/cards/CardListAdapter.java
@@ -2,12 +2,9 @@ package edu.temple.rollcall.cards;
 
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import edu.temple.rollcall.R;
-//import edu.temple.rollcall.util.CardCountDownTimer;
 
 import android.content.Context;
 import android.os.CountDownTimer;
@@ -26,6 +23,7 @@ public class CardListAdapter extends ArrayAdapter<Card> {
  
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        
         ViewHolder viewHolder;
         
         if(convertView == null) {
@@ -39,12 +37,10 @@ public class CardListAdapter extends ArrayAdapter<Card> {
             viewHolder.course_name = (TextView) convertView.findViewById(R.id.card_course_name);
             viewHolder.teacher_name = (TextView) convertView.findViewById(R.id.card_teacher_name);
             viewHolder.session_time = (TextView) convertView.findViewById(R.id.card_session_time);
-            viewHolder.timerText = (TextView) convertView.findViewById(R.id.card_countdown);
-            viewHolder.timer = new CardCountDownTimer(getItem(position).start_time_millis - new Date().getTime(), 1000, viewHolder.timerText);
-            viewHolder.timer.start();
+            viewHolder.countdown = (TextView) convertView.findViewById(R.id.card_countdown);
             convertView.setTag(viewHolder);
         } else {
-            // recycle the already inflated view 
+            // recycle the already inflated view
             viewHolder = (ViewHolder) convertView.getTag();
         }
         
@@ -55,9 +51,15 @@ public class CardListAdapter extends ArrayAdapter<Card> {
         viewHolder.teacher_name.setText(card.teacher_name);
         String sessionTime = card.day_of_week + ", " + card.start_time + " - " + card.end_time;
         viewHolder.session_time.setText(sessionTime);
-        if(new Date().getTime() >= card.start_time_millis) viewHolder.timerText.setText("In progress...");
-        
+        viewHolder.countdown.setText(card.timer.output);
+        if(viewHolder.timer != null) viewHolder.timer.cancel();
+        viewHolder.timer = new ViewHolderUpdateTimer(card.end_time_millis - new Date().getTime(), 1000, viewHolder.countdown, card, this);
+
         return convertView;
+    }
+    
+    public void removeTopCard() {
+    	this.remove(getItem(0));
     }
     
     /**
@@ -71,47 +73,33 @@ public class CardListAdapter extends ArrayAdapter<Card> {
         TextView course_name;
         TextView teacher_name;
         TextView session_time;
-        TextView timerText;
+        TextView countdown;
         
-        CardCountDownTimer timer;
+        ViewHolderUpdateTimer timer;
     }
     
-    public class CardCountDownTimer extends CountDownTimer {
+    private class ViewHolderUpdateTimer extends CountDownTimer {
 
-    	TextView view;
-    	
-    	public CardCountDownTimer(long millisInFuture, long countDownInterval, TextView view) {
+    	private TextView textView;
+    	private Card card;
+    	private CardListAdapter adapter;
+
+    	public ViewHolderUpdateTimer(long millisInFuture, long countDownInterval, TextView textView, Card card, CardListAdapter adapter) {
     		super(millisInFuture, countDownInterval);
-    		this.view = view;
+    		this.textView = textView;
+    		this.card = card;
+    		this.adapter = adapter;
+    		this.start();
     	}
 
     	@Override
     	public void onTick(long millisUntilFinished) {
-    		Long millis = millisUntilFinished;
-    		long days = TimeUnit.MILLISECONDS.toDays(millis);
-            millis -= TimeUnit.DAYS.toMillis(days);
-            long hours = TimeUnit.MILLISECONDS.toHours(millis);
-            millis -= TimeUnit.HOURS.toMillis(hours);
-            long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
-            if(days == 0) {
-            	String message = "Begins in";
-            	if(hours > 1) message += " " + hours + " hours";
-            	else if(hours == 1) message += " " + hours + " hour";
-            	if(minutes > 1) message += " " + minutes + " minutes.";
-            	else if(minutes == 1) message += " " + minutes + " minute.";
-            	if(hours == 0 && minutes == 0) message += " less than a minute.";
-            	this.view.setText(message);
-            } else {
-            	this.view.setVisibility(View.GONE);
-            }
+    		this.textView.setText(card.timer.output);
     	}
 
     	@Override
     	public void onFinish() {
-    		this.view.setText("In progress...");
+    		adapter.removeTopCard();
     	}
-    	
     }
-    
-    
 }
