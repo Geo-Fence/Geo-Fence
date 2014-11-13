e9f6639ce29ca64210ff6cedf1b4d259ded4b907
psalvatore <tuc45398@temple.edu>
2014-11-12 21:20:34 -0500

    Added teacher name and course time to session detail activity.

--- RollCall/src/edu/temple/rollcall/SessionDetailActivity.java
+++ RollCall/src/edu/temple/rollcall/SessionDetailActivity.java
@@ -1,7 +1,9 @@
 package edu.temple.rollcall;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import org.joda.time.DateTime;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,10 +14,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import edu.temple.rollcall.cards.Card.CardCountDownTimer;
 import edu.temple.rollcall.util.RollCallUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -27,7 +31,11 @@ import android.widget.TextView;
 public class SessionDetailActivity extends FragmentActivity {
 
 	private GoogleMap gMap;
+	
 	TextView course_name;
+	TextView teacher_name;
+	TextView session_time;
+	TextView timer;
 	Button check_in_button;
 	
 	@Override
@@ -38,6 +46,8 @@ public class SessionDetailActivity extends FragmentActivity {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 				
 		course_name = (TextView) findViewById(R.id.session_detail_course_name);
+		teacher_name = (TextView) findViewById(R.id.session_detail_teacher_name);
+		session_time = (TextView) findViewById(R.id.session_detail_session_time);
 		check_in_button = (Button) findViewById(R.id.session_detail_check_in_button);
 		check_in_button.setOnClickListener(buttonListener);
 
@@ -46,6 +56,14 @@ public class SessionDetailActivity extends FragmentActivity {
 		try {
 			sessionInfo = new JSONObject(intent.getExtras().getString("sessionInfo"));
 			course_name.setText(sessionInfo.getString("course_name"));
+			teacher_name.setText(sessionInfo.getString("teacher_first_name") + " " + sessionInfo.getString("teacher_last_name"));
+			
+			DateTime dateTime = new DateTime(Long.parseLong(sessionInfo.getString("start_time")) * 1000);
+			String sessionTime = dateTime.toString("E") + ", " + dateTime.toString("h:mm a") + " - ";
+			dateTime = dateTime.withMillis(Long.parseLong(sessionInfo.getString("end_time")) * 1000);
+			sessionTime = sessionTime + dateTime.toString("h:mm a");
+			session_time.setText(sessionTime);
+			
 			setUpMapIfNeeded(sessionInfo.getDouble("lat"), sessionInfo.getDouble("lng"));
 		} catch (JSONException e) {
 			e.printStackTrace();
@@ -75,6 +93,50 @@ public class SessionDetailActivity extends FragmentActivity {
             	gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 17));
             }
         }
+    }
+	
+	public class CardCountDownTimer extends CountDownTimer {
+
+    	public String output;
+
+    	public CardCountDownTimer(long millisInFuture, long countDownInterval) {
+    		super(millisInFuture, countDownInterval);
+    		this.start();
+    	}
+
+    	@Override
+    	public void onTick(long millisUntilFinished) {
+    		Long millis = millisUntilFinished;
+    		long days = TimeUnit.MILLISECONDS.toDays(millis);
+    		millis -= TimeUnit.DAYS.toMillis(days);
+    		long hours = TimeUnit.MILLISECONDS.toHours(millis);
+    		millis -= TimeUnit.HOURS.toMillis(hours);
+    		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+    	
+    		// Generates a grammatically correct phrase...
+    		String message = "Begins in ";
+    		if(days < 1) {
+    			if(hours > 0) {
+    				message += hours;
+    				message += (hours == 1) ? " hour" : " hours";
+    				message += (minutes > 0) ? ", " : ".";
+    			} else if(minutes == 0) {
+    				message += "less than a minute.";
+    			}
+    			if(minutes > 0) {
+    				message += minutes;
+    				message += (minutes == 1) ? " minute." : " minutes.";
+    			}
+    			this.output = message;
+    		} else {
+    			this.output = "";
+    		}
+    	}
+
+    	@Override
+    	public void onFinish() {
+    		this.output = "In progress...";
+    	}
     }
 	
 	@Override
